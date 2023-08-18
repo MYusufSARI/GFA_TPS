c9113b0e63dc00507c939e1a4a3bb36d526b850a
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2023-08-18 19:33:41 +0300

    .

--- Assets/Scripts/GFA/TPS/Movement/CharacterMovement.cs
+++ Assets/Scripts/GFA/TPS/Movement/CharacterMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,32 +6,34 @@ using UnityEngine;
 namespace GFA.TPS.Movement
 {
     [RequireComponent(typeof(CharacterController))]
-
     public class CharacterMovement : MonoBehaviour
     {
         private CharacterController _characterController;
 
-        //Karaktere gönderilen yürüme komutunu temsil ediyor.
-        public Vector2 MovementInput { get; set; }
-
         public Vector3 ExternalForces { get; set; }
 
+        public Vector2 MovementInput { get; set; }
+
         [SerializeField]
-        private float _movementSpeed;
+        private float _movementSpeed = 4;
+
+        public float Rotation { get; set; }
+
 
         private void Awake()
         {
             _characterController = GetComponent<CharacterController>();
         }
 
-
         private void Update()
         {
             var movement = new Vector3(MovementInput.x, 0, MovementInput.y);
 
-            _characterController.SimpleMove(movement *_movementSpeed +  ExternalForces);
+            transform.eulerAngles = new Vector3(0, Rotation);
+
+            _characterController.SimpleMove(movement * _movementSpeed + ExternalForces);
 
             ExternalForces = Vector3.Lerp(ExternalForces, Vector3.zero, 8 * Time.deltaTime);
-        } 
+        }
     }
 }
--- Assets/Scripts/GFA/TPS/Tests/MovementTest.cs
+++ Assets/Scripts/GFA/TPS/Tests/MovementTest.cs
@@ -2,22 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using GFA.TPS.Input;
+using GFA.TPS.Movement;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace GFA.TPS.Movement.Tests
 {
-
     public class MovementTest : MonoBehaviour
     {
-        [SerializeField]
-        private Vector2 _movementInput;
+        [SerializeField] private Vector2 _movementInput;
 
-        [SerializeField]
-        private CharacterMovement _characterMovement;
+        [SerializeField] private CharacterMovement _characterMovement;
 
-        [SerializeField]
-        private Vector3 _externalForceValue;
+        [SerializeField] private Vector3 _externalForceValue;
 
         private GameInput _gameInput;
 
@@ -25,7 +22,7 @@ namespace GFA.TPS.Movement.Tests
         {
             _gameInput = new GameInput();
         }
-
+        
         private void OnEnable()
         {
             _gameInput.Enable();
@@ -40,23 +37,19 @@ namespace GFA.TPS.Movement.Tests
             _gameInput.Player.Dodge.performed -= OnDodgeButtonPressed;
         }
 
-        private void OnDodgeButtonPressed(InputAction.CallbackContext context)
+        private void OnDodgeButtonPressed(InputAction.CallbackContext obj)
         {
             _characterMovement.ExternalForces += _externalForceValue;
         }
 
         private void Update()
         {
-            
-
-            if (UnityEngine.InputSystem.Keyboard.current.spaceKey.wasReleasedThisFrame)
-            {
-                _characterMovement.ExternalForces += _externalForceValue;
-            }
-
             var input = _gameInput.Player.Movement.ReadValue<Vector2>();
             _characterMovement.MovementInput = input;
-        }
 
+            _characterMovement.Rotation += Time.deltaTime * 10;
+        }
     }
+
+    
 }
