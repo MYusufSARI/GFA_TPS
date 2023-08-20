18815a2d175d0dfb795c9384b576e43467dfb9f4
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2023-08-20 20:54:37 +0300

    Added interfaces

---
+++ Assets/Scripts/GFA/TPS/ExplosiveBarrel.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using GFA.TPS;
+using UnityEngine;
+
+namespace GFA.TPS
+{
+
+    public class ExplosiveBarrel : MonoBehaviour, IDamagable
+    {
+        [SerializeField]
+        private float _health = 5;
+
+        [SerializeField]
+        private float explosionRadius = 5;
+
+        private float _explosionDamage = 5;
+
+        private float _explosionForce = 50;
+
+        [SerializeField]
+        private AnimationCurve _explosionFallOff;
+
+        public void ApplyDamage(float damage, GameObject causer = null)
+        {
+
+            _health -= damage;
+            if (_health <= 0)
+            {
+                Explode();
+            }
+        }
+
+        public void Explode()
+        {
+            var hits = Physics.OverlapSphere(transform.position, explosionRadius);
+
+            foreach (var hit in hits)
+            {
+                var distance = Vector3.Distance(transform.position, hit.transform.position);
+
+                var rate = distance / explosionRadius;
+
+                var fallOff = _explosionFallOff.Evaluate(rate);
+
+                if (hit.transform.TryGetComponent<IDamagable>(out var damagable))
+                {
+                    damagable.ApplyDamage(_explosionDamage + fallOff);
+                }
+
+                if (hit.attachedRigidbody)
+                {
+                    hit.attachedRigidbody.AddExplosionForce(_explosionForce, transform.position, explosionRadius, 1, ForceMode.Impulse);
+                }
+
+            }
+
+            
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GFA/TPS/IDamagable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GFA.TPS
+{
+    public interface IDamagable
+    {
+        void ApplyDamage(float damage, GameObject causer = null);
+    }
+}
