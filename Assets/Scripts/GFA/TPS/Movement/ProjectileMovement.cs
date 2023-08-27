bd6e983d2ea4e5e5c0cef571d0960635c9b21bfb
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2023-08-27 18:29:55 +0300

    Added ShooterTest file

--- Assets/Scripts/GFA/TPS/Movement/ProjectileMovement.cs
+++ Assets/Scripts/GFA/TPS/Movement/ProjectileMovement.cs
@@ -10,12 +10,42 @@ namespace GFA.TPS.Movement
     {
         [SerializeField]
         private float _speed;
-
         public float Speed
         {
             get => _speed;
             set => _speed = value;
-        } 
+        }
+
+
+        [SerializeField]
+        private bool _shouldDisableOnCollision;
+        public bool ShouldDisableOnColision
+        {
+            get => _shouldDisableOnCollision;
+            set => _shouldDisableOnCollision = value;
+        }
+
+
+        [SerializeField]
+        private bool _shouldDestroyOnCollision;
+        public bool ShouldDestroyOnColision
+        {
+            get => _shouldDestroyOnCollision;
+            set => _shouldDestroyOnCollision = value;
+        }
+
+
+        [SerializeField]
+        private bool _shouldBounce;
+        public bool ShouldBounce
+        {
+            get => _shouldBounce;
+            set => _shouldBounce = value;
+        }
+
+        [SerializeField]
+        private float _pushPower;
+
 
         private void Update()
         {
@@ -25,10 +55,31 @@ namespace GFA.TPS.Movement
 
             var targetPosition = transform.position + direction * distance;
 
+
             if (Physics.Raycast(transform.position, direction, out var hit, distance))
             {
+                if (hit.rigidbody)
+                {
+                    hit.rigidbody.AddForceAtPosition(-hit.normal * _speed * _pushPower, hit.point, ForceMode.Impulse);
+                }
+
+                if (ShouldDestroyOnColision)
+                {
+                    Destroy(gameObject);
+                }
+
+                if (ShouldDisableOnColision)
+                {
+                    enabled = false;
+                }
+
                 targetPosition = hit.point;
 
+                if (ShouldBounce)
+                {
+                    var reflectedDirection = Vector3.Reflect(direction, hit.normal);
+                    transform.forward = reflectedDirection; 
+                }
             }
 
             Debug.DrawLine(transform.position, targetPosition, Color.red);
---
+++ Assets/Scripts/GFA/TPS/Tests/ShooterTest.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GFA.TPS.Movement.Tests
+{
+
+    public class ShooterTest : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _projectilePrefab;
+
+        private void Start()
+        {
+            StartCoroutine(Shooter());
+        }
+
+        private IEnumerator Shooter()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(0.5f);
+
+                Instantiate(_projectilePrefab, transform.position, transform.rotation);
+            }
+        }
+
+    }
+}
